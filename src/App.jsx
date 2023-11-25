b5570ded3b86c091f38f79614ac35b32ab897d1c
Fernando Jimenez <fgjv06@gmail.com>
2023-11-25 12:09:21 -0300

    import Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-
+import Header from './components/Header'
 
 function App() {
   
   return (
     <>
-      <h1>Money Manager</h1>
+      <Header />
     </>
   )
 }
