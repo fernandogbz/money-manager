eebda7c130be59faf5093f334ae98783c0cedcc3
Fernando Jimenez <fgjv06@gmail.com>
2023-11-25 12:18:10 -0300

    form with style classes added

--- src/components/NewBudget.jsx
+++ src/components/NewBudget.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const NewBudget = () => {
   return (
     <div className='budget-container container shadow'>
+
+      <form className='form'>
       <h2>New Budget</h2>
+      </form>
     </div>
   )
 }
