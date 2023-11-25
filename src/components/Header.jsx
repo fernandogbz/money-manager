192f1ea5d26f86a2e7285e9395a4dd599d09f94e
Fernando Jimenez <fgjv06@gmail.com>
2023-11-25 12:12:02 -0300

    import NewBudget

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import NewBudget from './NewBudget'
 
 const Header = () => {
   return (
     <header>
       <h1>Money Manager</h1>
+      <NewBudget />
     </header>
   )
 }
