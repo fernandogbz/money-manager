cccb4955a7c023c34830701003613f8cd07b98f4
Fernando Jimenez <fgjv06@gmail.com>
2023-11-26 17:52:56 -0300

    Getting budget and setBudget from App.jsx. And passing them to NewBudget component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import NewBudget from './NewBudget'
 
-const Header = () => {
+const Header = ({budget, setBudget}) => {
   return (
     <header>
       <h1>Money Manager</h1>
-      <NewBudget />
+      <NewBudget 
+        budget={budget}
+        setBudget={setBudget}
+      />
     </header>
   )
 }
