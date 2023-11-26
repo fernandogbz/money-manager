a64fc2c97e40be8b2e68f4d57860077b8028fecd
Fernando Jimenez <fgjv06@gmail.com>
2023-11-26 18:32:47 -0300

    Creating a component that can take different types(if it's and error or it's correct), and passing the children with the full message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Message = () => {
+// Creating a component that can take different types(if it's and error or it's correct), and passing the children with the full message
+const Message = ({children, type}) => {
   return (
-    <div>
-      
+    <div className={`alert ${type}`}>{children}
     </div>
   )
 }
