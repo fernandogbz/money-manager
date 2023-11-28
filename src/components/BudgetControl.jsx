d3887aa5350cee50b0a6320fd2675a91d070c63f
Fernando Jimenez <fgjv06@gmail.com>
2023-11-28 17:18:40 -0300

    budget-content div

--- src/components/BudgetControl.jsx
+++ src/components/BudgetControl.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 
 const BudgetControl = () => {
   return (
-    <div className='container-budget container shadow two-columns'>
-      
+    <div className='budget-container container shadow two-columns'>
+      <div>
+        <p>Graph here</p>
+      </div>
+
+      <div className='budget-content'>
+        <p>
+          <span>Budget: </span> $200
+        </p>
+      </div>
     </div>
   )
 }
 
-export default BudgetControl className='container-budget container shadow two-columns'
+export default BudgetControl
