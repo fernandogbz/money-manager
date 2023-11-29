285dc877897aa1fe785d66cb25fbda6dbe4ddd8a
Fernando Jimenez <fgjv06@gmail.com>
2023-11-29 17:20:37 -0300

    Name changed to English

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,11 @@
+import CloseBtn from '../img/cerrar.svg'
+
 const Modal = () => {
   return (
     <div className="modal">
-      <div className="close-modal"></div>
+      <div className="close-modal">
+
+      </div>
     </div>
   )
 }
