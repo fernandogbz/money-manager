de276f4cd52ed323e9beb3376737904bcbbc22b2
Fernando Jimenez <fgjv06@gmail.com>
2023-11-29 17:26:00 -0300

    hideModal() function created, will be called when user clicks on closeBtn

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,19 @@
 import CloseBtn from '../img/close.svg'
 
 const Modal = () => {
+
+  const hideModal = () => {
+    console.log('Hiding...')
+  }
+
   return (
     <div className="modal">
       <div className="close-modal">
         <img 
         src={CloseBtn} 
-        alt="close modal" />
+        alt="close modal" 
+        onClick={hideModal}
+        />
       </div>
     </div>
   )
