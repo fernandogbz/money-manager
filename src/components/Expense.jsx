d6cc70b121e7e666d808b3ca5fc51422df61253a
Fernando Jimenez <fgjv06@gmail.com>
2023-12-05 16:21:40 -0300

    Destructuring expense

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 const Expense = ({expense}) => {
+  const {category, name, amount, id} = expense; //Destructuring expense
+
   return (
     <div className='expense shadow'>
       <div className='content-expense'>
 
         <div className='description-expense'>
           <p className='category'>
-            {expense.category}
+            {category}
           </p>
         </div>
       </div>
