af0a671bd32c57f9f61a46c42248fa8c0e4542c4
Fernando Jimenez <fgjv06@gmail.com>
2023-12-05 15:53:01 -0300

    expenses.map to iterate in expenses (map execute at least one time for each item, if there aren't don't execute), and read Expense component

--- src/components/ExpensesList.jsx
+++ src/components/ExpensesList.jsx
@@ -4,6 +4,11 @@ const ExpensesList = ({expenses}) => {
   return (
     <div className='expenses-list container'>
       <h2>{expenses.length ? 'Expenses' : 'There are no expenses yet'}</h2>
+
+      {expenses.map( expense => {
+        <Expense />
+      }) // map execute at least one time for each item, if there aren't don't execute
+      }
     </div>
   )
 }
