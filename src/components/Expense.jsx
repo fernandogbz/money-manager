35042370cf5540c07a88c70147cc41893de7d866
Fernando Jimenez <fgjv06@gmail.com>
2023-12-05 20:36:58 -0300

    import formatDate() from helpers, and p with date in the proper format added

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import {newDate} from '../helpers'
+import {formatDate} from '../helpers'
 
 const Expense = ({expense}) => {
-  const {category, name, amount, id} = expense; //Destructuring expense
+  const {category, name, amount, id, date} = expense; //Destructuring expense
 
   return (
     <div className='expense shadow'>
@@ -11,6 +11,10 @@ const Expense = ({expense}) => {
         <div className='description-expense'>
           <p className='category'>{category}</p>
           <p className='expense-name'>{name}</p>
+          <p className='expense-date'>
+            Added on: {''}
+            <span>{formatDate(date)}</span>
+          </p>
         </div>
       </div>
     </div>
