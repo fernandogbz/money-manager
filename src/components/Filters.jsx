d2e62389ae427be1d46a7081b25cf52cc49262ac
fernandogbz <fgjv06@gmail.com>
2024-01-04 17:46:51 -0300

    select with options added

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -6,6 +6,16 @@ const Filters = () => {
             <form>
                 <div className="field">
                     <label>Filter Expenses</label>
+                    <select>
+                        <option value="">--Select--</option>
+                        <option value="savings">Savings</option>
+                        <option value="food">Food</option>
+                        <option value="home">Home</option>
+                        <option value="expenses">Miscellaneous Expenses</option>
+                        <option value="leisure">Leisure</option>
+                        <option value="health">Health</option>
+                        <option value="subscriptions">Subscriptions</option>
+                    </select>
                 </div>
             </form>
         </div>
