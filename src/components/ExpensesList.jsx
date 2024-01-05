387927481ab45df6fb5837135b3c2d798f74a8a6
fernandogbz <fgjv06@gmail.com>
2024-01-05 18:53:48 -0300

    h2 Expenses/There are no expenses, implemented to filters and all expenses if there are no filters

--- src/components/ExpensesList.jsx
+++ src/components/ExpensesList.jsx
@@ -10,27 +10,36 @@ const ExpensesList = ({
 }) => {
   return (
     <div className='expenses-list container'>
-      <h2>{expenses.length ? 'Expenses' : 'There are no expenses yet'}</h2>
 
       { //If there's something in filter then iterate over the filtered expenses
-        filter ? (
-          filteredExpenses.map( expense => ( //// map execute at least one time for each item, if there aren't don't execute
-            <Expense 
-              key={expense.id}
-              expense={expense}
-              setEditExpense={setEditExpense}
-              deleteExpense={deleteExpense}
-            />
-          ))
-        ) : ( // If there's nothing in filter iterate over all the expenses
-          expenses.map( expense => ( //// map execute at least one time for each item, if there aren't don't execute
-            <Expense 
-              key={expense.id}
-              expense={expense}
-              setEditExpense={setEditExpense}
-              deleteExpense={deleteExpense}
-            />
-          ))
+      filter ? (
+         <>
+            <h2>{filteredExpenses.length ? 'Expenses' : 'There are no expenses in this category'}</h2>
+            {
+               filteredExpenses.map( expense => ( // map execute at least one time for each item, if there aren't don't execute
+               <Expense 
+                  key={expense.id}
+                  expense={expense}
+                  setEditExpense={setEditExpense}
+                  deleteExpense={deleteExpense}
+               />
+               ))
+            }
+         </>
+         ) : ( // If there's nothing in filter iterate over all the expenses
+         <>
+            <h2>{expenses.length ? 'Expenses' : 'There are no expenses yet'}</h2>
+               {
+                  expenses.map( expense => (
+                     <Expense 
+                     key={expense.id}
+                     expense={expense}
+                     setEditExpense={setEditExpense}
+                     deleteExpense={deleteExpense}
+                     />
+                  ))
+               }
+         </>
         ) 
       }
     </div>
