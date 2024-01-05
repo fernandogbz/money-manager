6a38caae5f366cd6beab3b7b56530e1eec4cac9c
fernandogbz <fgjv06@gmail.com>
2024-01-05 18:33:22 -0300

    If there's something in filter then iterate over the filtered expenses, otherwise (if filter is empty) iterate over all the expenses

--- src/components/ExpensesList.jsx
+++ src/components/ExpensesList.jsx
@@ -12,15 +12,26 @@ const ExpensesList = ({
     <div className='expenses-list container'>
       <h2>{expenses.length ? 'Expenses' : 'There are no expenses yet'}</h2>
 
-      {expenses.map( expense => ( //// map execute at least one time for each item, if there aren't don't execute
-
-        <Expense 
-          key={expense.id}
-          expense={expense}
-          setEditExpense={setEditExpense}
-          deleteExpense={deleteExpense}
-        />
-      ))
+      { //If there's something in filter then iterate over the filtered expenses
+        filter ? (
+          filteredExpenses.map( expense => ( //// map execute at least one time for each item, if there aren't don't execute
+            <Expense 
+              key={expense.id}
+              expense={expense}
+              setEditExpense={setEditExpense}
+              deleteExpense={deleteExpense}
+            />
+          ))
+        ) : ( // If there's nothing in filter iterate over all the expenses
+          expenses.map( expense => ( //// map execute at least one time for each item, if there aren't don't execute
+            <Expense 
+              key={expense.id}
+              expense={expense}
+              setEditExpense={setEditExpense}
+              deleteExpense={deleteExpense}
+            />
+          ))
+        ) 
       }
     </div>
   )
